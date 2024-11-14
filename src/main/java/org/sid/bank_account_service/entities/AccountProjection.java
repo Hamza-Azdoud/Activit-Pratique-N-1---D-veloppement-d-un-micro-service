f0ccb6880ad30d1f054c3cb5994c41afdaa20731
Hamza-Azdoud <h.azdoud@etu.enset-media.ac.ma>
2024-11-14 17:30:54 +0100

    la Couche Service

--- src/main/java/org/sid/bank_account_service/entities/AccountProjection.java
+++ src/main/java/org/sid/bank_account_service/entities/AccountProjection.java
@@ -3,7 +3,7 @@ package org.sid.bank_account_service.entities;
 import org.sid.bank_account_service.enums.AccountType;
 import org.springframework.data.rest.core.config.Projection;
 
-@Projection(types = BankAccount.class, name = "p1")
+@Projection(types = BankAccount.class, name = "p2")
 public interface AccountProjection {
         public String getId();
         public AccountType getAccountType();
---
+++ src/main/java/org/sid/bank_account_service/services/AccountServices.java
@@ -0,0 +1,9 @@
+package org.sid.bank_account_service.services;
+
+import org.sid.bank_account_service.dto.BankAccountRequestDTO;
+import org.sid.bank_account_service.dto.BankAccountResponesDTO;
+import org.sid.bank_account_service.entities.BankAccount;
+
+public interface AccountServices {
+    public BankAccountResponesDTO addAccount(BankAccountRequestDTO account);
+}
